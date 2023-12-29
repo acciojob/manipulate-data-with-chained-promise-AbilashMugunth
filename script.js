8c01e1d269e5a2addd1a77cb8f33f69c3d3f91ae
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-29 21:00:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+let output = document.querySelector("#output");
+
+async function filterArr(arr) {
+  const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(arr.filter((el) => el % 2 !== 0));
+    }, 1000);
+  });
+
+  const data = await p1;
+
+  const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(
+        data.map((element) => {
+          return element * 2;
+        })
+      );
+    }, 2000);
+  });
+
+  const final = await p2;
+  output.innerHTML = final;
+}
